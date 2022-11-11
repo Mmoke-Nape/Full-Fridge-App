e53bd07abe06fb87c5443a585afe46916e88c0ac
Mmoke-Nape <napemmoke@gmail.com>
2022-11-12 01:48:26 +0200

    Iitial Commit

---
+++ lib/constants/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // primary and secondary palette
+  static const backgroundPageColor = Color(0xfff5f1f2);
+  static const backgroundFieldsColor = Color(0xffebe1e4);
+  static const activeColor = Color(0xffc4a2ad);
+  static const accentColor = Color(0xff604859);
+}
